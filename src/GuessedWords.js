5e0d63b20917860cb703adca7fd7e55ce55d9362
DiangoGav <diango.gavidia@sitiouno.com>
2020-02-19 18:12:52 -0400

    GuessedWords ...

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -8,6 +8,21 @@ const GuessedWords = (props) => {
       <div data-test='guess-instructions'>
         <span>Instructions</span>
       </div>
+  } else {
+    content =
+      <div data-test='guessed-words'>
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th><th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
+      </div>
   }
   return (
     <div data-test='component-guessed-words'>
