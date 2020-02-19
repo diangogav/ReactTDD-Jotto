b27d20b87edd7f4f40d3cbedbae4cfa12fa24aee
DiangoGav <diango.gavidia@sitiouno.com>
2020-02-19 19:32:23 -0400

    Finish GuessedWords test

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -1,35 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const GuessedWords = (props) => {
+const GuessedWords = props => {
   let content;
   if (props.guessedWords.length === 0) {
-    content =
-      <div data-test='guess-instructions'>
+    content = (
+      <div data-test="guess-instructions">
         <span>Instructions</span>
       </div>
+    );
   } else {
-    content =
-      <div data-test='guessed-words'>
+    content = (
+      <div data-test="guessed-words">
         <h3>Guessed Words</h3>
         <table>
           <thead>
             <tr>
-              <th>Guess</th><th>Matching Letters</th>
+              <th>Guess</th>
+              <th>Matching Letters</th>
             </tr>
           </thead>
           <tbody>
-
+            {props.guessedWords.map((word, index) => (
+              <tr key={index} data-test="guessed-word">
+                <td>{word.guessedWord}</td>
+                <td>{word.letterMatchCount}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
+    );
   }
-  return (
-    <div data-test='component-guessed-words'>
-      {content}
-    </div>
-  )
-}
+  return <div data-test="component-guessed-words">{content}</div>;
+};
 
 GuessedWords.propTypes = {
   guessedWords: PropTypes.arrayOf(
@@ -38,7 +42,6 @@ GuessedWords.propTypes = {
       letterMatchCount: PropTypes.number.isRequired
     })
   ).isRequired
-}
-
+};
 
 export default GuessedWords;
