c715c241d09f441faab93bcec18748adfda7af60
DiangoGav <diango.gavidia@sitiouno.com>
2020-02-19 22:44:26 -0400

    base for Input testing

---
+++ src/Input.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr } from '../test/testUtils';
+
+describe('render', () => {
+  describe('word has not been guessed', () => {
+    test('renders component without error', () => {});
+
+    test('render input box', () => {});
+
+    test('render submit button', () => {});
+  });
+
+  describe('word has been guessed', () => {
+    test('renders component without error', () => {});
+
+    test('does not render input box', () => {});
+
+    test('does not render submit button', () => {});
+  });
+});
+
+describe('update state', () => {});
