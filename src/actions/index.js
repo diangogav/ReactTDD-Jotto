823f4dbba26c499d7404c18f669b26107095a708
DiangoGav <diango.gavidia@sitiouno.com>
2020-02-20 23:48:41 -0400

    add  action creator, add thunk for  piece of state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,23 @@
+import { getLetterMatchCount } from '../helpers/index';
 export const actionTypes = {
-  CORRECT_GUESS: 'CORRECT_GUESS'
+  CORRECT_GUESS: 'CORRECT_GUESS',
+  GUESS_WORD: 'GUESS_WORD'
 };
 
-export function correctGuess() {
-  return { type: actionTypes.CORRECT_GUESS };
+export function guessWord(guessedWord) {
+  return function(dispatch, getState) {
+    const { secretWord } = getState();
+    const letterMatchCount = getLetterMatchCount(secretWord, guessedWord);
+
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount }
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({
+        type: actionTypes.CORRECT_GUESS
+      });
+    }
+  };
 }
